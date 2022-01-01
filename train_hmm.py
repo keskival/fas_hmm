6d0d3151f498b688d3695f3e54bdbf6029f2d9d7
Tero Keski-Valkama <tero.keski-valkama@neter.fi>
2022-01-01 16:26:54 +0100

    Plotting the scores from the hidden state KL divergences instead.

--- train_hmm.py
+++ train_hmm.py
@@ -38,6 +38,7 @@ def get_n_sequences(n, correct=True, validation=False):
     return runs
 
 scores = []
+direct_scores = []
 hidden_states_sequence = []
 
 for number_of_hidden_states in range(1, NUMBER_OF_HIDDEN_STATES):
@@ -130,15 +131,22 @@ for number_of_hidden_states in range(1, NUMBER_OF_HIDDEN_STATES):
     log_probabilities_positive_for_positive = correct_log_probs
     log_probabilities_positive_for_negative = faulty_log_probs
 
-    y_true = np.concatenate([np.ones_like(log_probabilities_positive_for_positive), np.zeros_like(log_probabilities_positive_for_negative)])
-    y_pred = np.concatenate([log_probabilities_positive_for_positive, log_probabilities_positive_for_negative])
+    y_true = np.concatenate([np.zeros_like(correct_distribution_differences), np.ones_like(log_probabilities_positive_for_negative)])
+    y_pred = np.concatenate([correct_distribution_differences, log_probabilities_positive_for_negative])
     roc_auc_score = sklearn.metrics.roc_auc_score(y_true, y_pred)
 
-    print(f"Number of hidden states: {number_of_hidden_states}, Correct log prob min,mean,max: {correct_log_prob}, faulty log prob min,mean,max: {faulty_log_prob}, ROC AUC score: {roc_auc_score}, correct hidden state distribution diffs: {correct_distribution_diff}, faulty hidden state distribution diffs: {faulty_distribution_diff}")
+    y_true = np.concatenate([np.ones_like(log_probabilities_positive_for_positive), np.zeros_like(erroneous_distribution_differences)])
+    y_pred = np.concatenate([log_probabilities_positive_for_positive, log_probabilities_positive_for_negative])
+    roc_auc_score_direct = sklearn.metrics.roc_auc_score(y_true, y_pred)
+
+    print(f"Number of hidden states: {number_of_hidden_states}, Correct log prob min,mean,max: {correct_log_prob}, faulty log prob min,mean,max: {faulty_log_prob}, correct hidden state distribution diffs: {correct_distribution_diff}, faulty hidden state distribution diffs: {faulty_distribution_diff}")
+    print(f"ROC AUC score direct: {roc_auc_score_direct}, ROC AUC score: {roc_auc_score}")
     hidden_states_sequence.append(number_of_hidden_states)
+    direct_scores.append(roc_auc_score_direct)
     scores.append(roc_auc_score)
 print(f"hidden_states={hidden_states_sequence}")
+print(f"direct_scores={direct_scores}")
 print(f"scores={scores}")
 
-plt.plot(hidden_states, scores)
+plt.plot(hidden_states_sequence, scores)
 plt.show()
